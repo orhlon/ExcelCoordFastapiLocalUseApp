7fdbfa162d46a619a88012214a925893a3b9b1d9
orhlon <61586410+orhlon@users.noreply.github.com>
2025-05-28 00:30:54 +0300

    Update install.bat

--- install.bat
+++ install.bat
@@ -18,5 +18,7 @@ echo "Запустить run.bat, затем запустить ExcelAddCoordsAp
 (
 echo uvicorn ExcelAddCoordsApp:app --reload
 )>> "C:\Users\%USERNAME%\Desktop\ExcelAddCoordsApp\run.bat"
+curl "https://github.com/orhlon/ExcelCoordFastapiLocalUseApp/raw/refs/heads/main/%D0%B1%D0%B0%D0%B7%D0%B0%20%D0%BA%D0%BE%D0%BE%D1%80%D0%B4%D0%B8%D0%BD%D0%B0%D1%82.xlsx" -o "%TEMP%\BazaCoordinat.xlsx"
+copy %TEMP%\BazaCoordinat.xlsx C:\Users\%USERNAME%\Desktop\ExcelAddCoordsApp\BazaCoordinat.xlsx
 copy %TEMP%\ExcelAddCoordsApp.py C:\Users\%USERNAME%\Desktop\ExcelAddCoordsApp\logic\ExcelAddCoordsApp.py
 pip install fastapi uvicorn openpyxl geopy python-multipart
